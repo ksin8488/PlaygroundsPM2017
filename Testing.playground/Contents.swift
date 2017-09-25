72727979083bf2e78ee1a75bfa06c3088a7018c5
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-09-25 12:07:30 -0600

    Created a Repo for PlaygroundsPM2017 and changed .gitignore file.

---
+++ Testing.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var myName :String  //explicit typing
+var otherName = "Words"	//implicit typing
+//both declares the var's as strings
+var thisName :String = "meow"   //explicit AND implicit typing
+
+//This is swift 3so Swift 4 features will not be supported
+
+
+
+
+
