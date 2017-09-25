64d65f223f57f969eaf584ab0ab97b6ec7d2a0bc
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-09-25 12:09:33 -0600

    Stupid commit + text - added some coding in creating implicit and explicit types for string.

--- Testing.playground/Contents.swift
+++ Testing.playground/Contents.swift
@@ -9,6 +9,7 @@ var otherName = "Words"	//implicit typing
 //both declares the var's as strings
 var thisName :String = "meow"   //explicit AND implicit typing
 
+
 //This is swift 3so Swift 4 features will not be supported
 
 
