18de406895d4a345bfac7b771adada2f7d15c1c5
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-09-25 12:38:06 -0600

    Started the Playgrounds with Strings assignments and added the link to an online resource as well as create examples with code on how to do a String Change, String Interpolation, and now starting the Counting Characters.

--- Testing.playground/Contents.swift
+++ Testing.playground/Contents.swift
@@ -13,6 +13,18 @@ var thisName :String = "meow"   //explicit AND implicit typing
 //This is swift 3so Swift 4 features will not be supported
 
 
+//Playgrounds with String assignment
+//https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/StringsAndCharacters.html#//apple_ref/doc/uid/TP40014097-CH7-ID292 was a very useful resource in doing these
 
+//String Change
+var treeType :String
+treeType = "Aspen"
+treeType = "Oak"
+
+//String Interpolation - usings strings to insert constants, variables, literals, and expressions into longer strings
+var treeAge = 10
+let message = "The hight of the tree is the age \(treeAge) times 5.9 which is \(Double(treeAge) * 5.9)" //This is not true IRL
+
+//Counting Characters - use the count property of the string
 
 
