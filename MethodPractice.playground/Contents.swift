ec8a79c4217b62ecfd605371049069ee5bd385e0
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-09-27 12:11:48 -0600

    Added more examples of methods and how tey can be used in comparing such as an if statement and a while loop.

--- MethodPractice.playground/Contents.swift
+++ MethodPractice.playground/Contents.swift
@@ -17,3 +17,31 @@ aBitLessSimple(name: "slim shady")
 let words = "Marshall Mathers"
 aBitLessSimple(name: words)
 
+public func namingExample(outerName innerName : String) -> Void //the colon having a space doesn't matter
+{
+    print("The inner name is: \(innerName)")
+}
+namingExample(outerName: "Cody")
+
+public func counting() -> Int
+{
+    return (7 * 665)
+}
+print("I am not \(counting()) years old") //can use interpolation
+
+if(counting() < 10)
+{
+    print("math works in swift too")
+}
+else
+{
+    print("not very likely right now")
+}
+var test = 0
+while(test < counting())    //method MUST have the parens () on them
+{
+    print("hahahah")
+    test += 1
+}
+
+print("yay")
