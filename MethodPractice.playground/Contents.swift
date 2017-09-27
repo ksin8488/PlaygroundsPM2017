64ed3093b8ac261e0ad2830ecf4bae1cd3dd0017
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-09-27 11:56:47 -0600

    created some method headers. One just prints someting when called and the other displays what string was typed as the return.

---
+++ MethodPractice.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+public func isReallySimple() -> Void
+{
+    print("I'm excited to be using my laptop computer more")
+}
+isReallySimple()
+
+public func aBitLessSimple(name: String) -> Void
+{
+    let answer = "My name is not"  + name
+    print(answer)
+}
+aBitLessSimple(name: "slim shady")
+let words = "Marshall Mathers"
+aBitLessSimple(name: words)
+
